9aa0b76f5fd7efef9d4f50c79553ce2c8793093b
test-bot-69 <testbot.1831@gmail.com>
2024-01-29 14:15:36 +0530

    Added mongoDB Atlas

--- inotebook-backend/db.js
+++ inotebook-backend/db.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI =
+  'mongodb+srv://omkar-ghodake:Omkar%401831%24@cluster0.w5b4a.mongodb.net/SecurityBoatAssignment?retryWrites=true&w=majority'
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected to Mongo Successfully");
-    })
+const connectToMongo = () => {
+  mongoose.connect(mongoURI, () => {
+    console.log('Connected to Mongo Successfully')
+  })
 }
 
-module.exports = connectToMongo;
+module.exports = connectToMongo
