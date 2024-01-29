fa0391cf3ed7306243d8a7e7338fcf1cd1f6462d
test-bot-69 <testbot.1831@gmail.com>
2024-01-29 15:10:16 +0530

    asilfs

--- inotebook-backend/index.js
+++ inotebook-backend/index.js
@@ -18,3 +18,20 @@ app.listen(port, () => {
     `App listening at https://your-notebook-backend.vercel.app:${port}`
   )
 })
+
+// Your code
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, 'client', 'build', 'index.html'),
+      function (err) {
+        if (err) {
+          res.status(500).send(err)
+        }
+      }
+    )
+  })
+}
+// Your code
